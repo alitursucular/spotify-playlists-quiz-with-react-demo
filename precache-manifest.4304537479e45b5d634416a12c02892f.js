d4a0b3c2d4c57a2d334fabb5651f904c97f2ffb8
Ali Turşucular <alitursucular@gmail.com>
2019-05-29 18:35:10 +0100

    Updates

---
+++ precache-manifest.4304537479e45b5d634416a12c02892f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b652edf6edf8564f29854f756b7c82e3",
+    "url": "/spotify-playlists-quiz-with-react-demo/index.html"
+  },
+  {
+    "revision": "90e65f7773f9f3ed49e3",
+    "url": "/spotify-playlists-quiz-with-react-demo/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "f0f8a33b1f4cffbffb54",
+    "url": "/spotify-playlists-quiz-with-react-demo/static/css/main.670ddb61.chunk.css"
+  },
+  {
+    "revision": "90e65f7773f9f3ed49e3",
+    "url": "/spotify-playlists-quiz-with-react-demo/static/js/2.33f4a4b4.chunk.js"
+  },
+  {
+    "revision": "f0f8a33b1f4cffbffb54",
+    "url": "/spotify-playlists-quiz-with-react-demo/static/js/main.06a44758.chunk.js"
+  },
+  {
+    "revision": "22c76759a05ea8c727c4",
+    "url": "/spotify-playlists-quiz-with-react-demo/static/js/runtime~main.d396925d.js"
+  },
+  {
+    "revision": "27f8ae7d4ccf210d67952985ad607d82",
+    "url": "/spotify-playlists-quiz-with-react-demo/static/media/add.27f8ae7d.svg"
+  },
+  {
+    "revision": "4a7e38a82752e102fc1a999679b97b7e",
+    "url": "/spotify-playlists-quiz-with-react-demo/static/media/react_logo.4a7e38a8.svg"
+  },
+  {
+    "revision": "977b3a3cdbf5f274d24952111f3e4975",
+    "url": "/spotify-playlists-quiz-with-react-demo/static/media/spotify_logo.977b3a3c.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/spotify-playlists-quiz-with-react-demo/precache-manifest.4304537479e45b5d634416a12c02892f.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/spotify-playlists-quiz-with-react-demo/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
